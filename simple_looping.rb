43a875555bd41ae464ce4d076d7873f84596a1de
Cassandra Montoya <montoya.c@outlook.com>
2018-07-13 00:53:57 +0000

    automatically backed up by learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -1,6 +1,13 @@
 # REMEMBER: print your output to the terminal using 'puts'
 
 def loop_iterator(number_of_times)
+  number_of_times = 0
+  loop do
+    puts "Welcome to Flatiron School's Web Development Course!"
+    number_of_times = + 1
+    if number_of_times > = 7
+      break
+    end
   # Code your solution here using the "loop" keyword to puts out the below phrase
   # This and all the other methods take an argument of an integer 
   # The integer is the number of times the loops should puts out the phrase
@@ -9,34 +16,41 @@ def loop_iterator(number_of_times)
   # Maybe we should keep count of the number of times we've puts out the 
   #  phrase and break when the counter hits the appropriate number...
   phrase = "Welcome to Flatiron School's Web Development Course!"
-  
 end
 
 def times_iterator(number_of_times)
+  7.times do
+    puts "Welcome to Flatiron School's Web Development Course!"
   # code your solution here using the "times" keyword
   phrase = "Welcome to Flatiron School's Web Development Course!"
   
 end
 
 def while_iterator(number_of_times)
+  number_of_times = 0
+  while number_of_times < 7
+  number_of_times + = 1 
+  puts "Welcome to Flatiron School's Web Development Course!"
   # code your solution here using the "while" keyword
   # hint: user a counter to tell the while loop when to stop!
   phrase = "Welcome to Flatiron School's Web Development Course!"
-  
 end
 
 def until_iterator(number_of_times)
+  number_of_times= 0
+  until number_of_times ==7
+  puts "Welcome to Flatiron School's Web Development Course!"
+  number_of_times + = 1
   # code your solution here using the "until" keyword
   # hint: use a counter to tell the until loop when to stop!
   phrase = "Welcome to Flatiron School's Web Development Course!"
-
 end
 
 def for_iterator(number_of_times)
+  for number_of_times 1..7 do
+    puts "Welcome to Flatiron School's Web Development Course!"
   # code your solution here using the "for" keyword
   # remember that `for` requires a range of numbers. How can we turn our number into a range?
-  
   phrase = "Welcome to Flatiron School's Web Development Course!"
-  
 end
 
